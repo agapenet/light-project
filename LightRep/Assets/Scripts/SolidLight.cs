fd000651676c5e37d82affd14d7a2909a435b92d
agapenet <lossnotfunny@gmail.com>
2023-02-16 10:19:59 -0600

    bridge-work
    it doesn't work just yet

--- LightRep/Assets/Scripts/SolidLight.cs
+++ LightRep/Assets/Scripts/SolidLight.cs
@@ -6,15 +6,51 @@ using UnityEngine.Rendering.Universal;
 
 public class SolidLight : MonoBehaviour
 {
-    public float intensity;
+    public float duration;
+    private float fadeTime;
+    public bool activated = false;
+    public UnityEngine.Experimental.Rendering.Universal.Light2D bridge;
 
     private void Start()
     {
-        
+        bridge.intensity = 0;
     }
 
     private void Update()
     {
-        
+        fadeTime = Time.deltaTime / duration;
+    }
+
+    public void ToggleState()
+    {
+       //not working yet
+        if (activated == false)
+        {
+            Debug.Log("Starting fade in");
+            StartCoroutine(FadeIn());
+            return;
+        }
+
+        if (activated == true)
+        {
+            Debug.Log("Starting fade in");
+            StartCoroutine(FadeOut());
+            return;
+        }
+       // StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        bridge.intensity = Mathf.Lerp(0, 1, fadeTime);
+        yield return null;
+        activated = true;
+    }
+
+    IEnumerator FadeOut()
+    {
+        bridge.intensity = Mathf.Lerp(1, 0, fadeTime);
+        yield return null;
+        activated = false;
     }
 }
