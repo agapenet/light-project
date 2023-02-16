68f98b6564116fe63c91c27e5cd45f6d5ed16571
agapenet <125381528+agapenet@users.noreply.github.com>
2023-02-16 00:06:51 -0600

    Update TheLab.cs

--- LightRep/Assets/Scripts/TheLab.cs
+++ LightRep/Assets/Scripts/TheLab.cs
@@ -6,5 +6,13 @@ using UnityEngine.Rendering.Universal;
 public class TheLab : MonoBehaviour
 {
     public float intensity;
-    
+    public UnityEngine.Rendering.Universal.Light2D light;
+    //might need to add experimental idk
+
+    //then use something like this:
+    public void start()
+    {
+        light = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        light.intensity = 1;
+    }
 }
