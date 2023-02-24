5f77e6914859b1ba6794dd1ed0954cdee1390486
agapenet <lossnotfunny@gmail.com>
2023-02-24 10:37:16 -0600

    th

--- LightRep/Assets/Scripts/WL_Point.cs
+++ LightRep/Assets/Scripts/WL_Point.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class WL_Point : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject guidePrefab;
+    private GameObject guide;
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateGuide()
     {
-        
+        guide = Instantiate<GameObject>(guidePrefab, transform.position, transform.rotation);
     }
 }
--- LightRep/Assets/Scripts/WorldLight.cs
+++ LightRep/Assets/Scripts/WorldLight.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Rendering.Universal;
 
 public class WorldLight : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private UnityEngine.Experimental.Rendering.Universal.Light2D guideLight;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
+    {
+        guideLight = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        guideLight.intensity = 0;
+
+        if(gameObject != null)
+        {
+           guideLight.intensity = 1;
+        }
+    }
+
+    private void OnEnable()
     {
         
     }
