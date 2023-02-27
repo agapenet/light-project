764e1cbec13ce895dabf531b7613b32909dfcb6c
agapenet <lossnotfunny@gmail.com>
2023-02-27 09:55:58 -0600

    owned
    glub

--- LightRep/Assets/Scripts/SolidLight.cs
+++ LightRep/Assets/Scripts/SolidLight.cs
@@ -9,7 +9,8 @@ public class SolidLight : MonoBehaviour
     [SerializeField] private Collider2D bridgeCollider;
 
     public float duration;
-    private float fadeTime;
+    public float fadeSpeed;
+    private float fadeAmount;
     public bool activated = false;
     public UnityEngine.Experimental.Rendering.Universal.Light2D bridge;
 
@@ -21,7 +22,17 @@ public class SolidLight : MonoBehaviour
 
     private void Update()
     {
-        fadeTime = Time.deltaTime / duration;
+        if(activated == false && bridge.intensity >= 0)
+        {
+            fadeAmount = bridge.intensity - (fadeSpeed * Time.deltaTime);
+            bridge.intensity = fadeAmount;
+        }
+        
+        if(activated == true && bridge.intensity <= 1)
+        {
+            fadeAmount = bridge.intensity + (fadeSpeed * Time.deltaTime);
+            bridge.intensity = fadeAmount;
+        }
     }
 
     public void ToggleState()
@@ -30,7 +41,7 @@ public class SolidLight : MonoBehaviour
         if (activated == false)
         {
             Debug.Log("Starting fade in");
-            bridge.intensity = 1;
+            //bridge.intensity = 1;
             bridgeCollider.enabled = true;
             activated = true;
             return;
@@ -39,28 +50,10 @@ public class SolidLight : MonoBehaviour
         if (activated == true)
         {
             Debug.Log("Starting fade out");
-            bridge.intensity = 0;
+            //bridge.intensity = 0;
             bridgeCollider.enabled = false;
             activated = false;
             return;
         }
-       // return;
-       // StartCoroutine(FadeIn());
-    }
-
-    public void FadeIn()
-    {
-        bridge.intensity = Mathf.Lerp(0, 1, fadeTime);
-        activated = true;
-        return;
-        //gave up on this
-    }
-
-    public void FadeOut()
-    {
-        bridge.intensity = Mathf.Lerp(1, 0, fadeTime);
-        activated = false;
-        return;
-        //gave up on this
     }
 }
